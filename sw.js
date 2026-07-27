0fac3b15752339fd91cd83cc1ed10a0d7c255369
oleeducks-source <oleeducks@gmail.com>
2026-07-27 02:39:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 // IMPORTANT : le numéro de version ci-dessous doit être incrémenté à
 // chaque mise à jour de ce fichier pour forcer le navigateur à détecter
 // un nouveau service worker et à vider l'ancien cache.
-const CACHE_NAME = "oleeducks-shell-v4";
+const CACHE_NAME = "oleeducks-shell-v5";
 const SHELL_FILES = [
   "./",
   "./index.html",
@@ -24,6 +24,7 @@ const SHELL_FILES = [
   "./js/comptabilite.js",
   "./js/pieces-jointes.js",
   "./js/rapport.js",
+  "./js/notifications.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
